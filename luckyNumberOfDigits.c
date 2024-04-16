b07fe6e0f94b5af66ff560ccf6be7d3d3a62a7db
asus test <msa-99@outlook.com>
2024-04-16 18:58:57 +0600

    lucky numbers of a digit

---
+++ luckyNumberOfDigits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+      int givenNumber, fiD, seD;
+      scanf("%d",&givenNumber);
+      fiD=givenNumber/10;
+      seD=givenNumber%10;
+      if(fiD%seD==0 || seD%fiD==0){
+        printf("Yes");
+      }else{
+        printf("No");
+      }
+    //   printf("fd-%d\nsd-%d",fiD,seD);
+    return 0;
+}
