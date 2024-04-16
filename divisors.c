0012fc6937f75a739c1bd95b4bce834cf1d26522
asus test <msa-99@outlook.com>
2024-04-16 18:41:01 +0600

    all divisors of a given number

---
+++ divisors.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<limits.h>
+//int i=n; i<=1; i--
+int main(){
+      int n;
+      scanf("%d",&n);
+      for( int i = 1; i<=n; i++){
+         if(n%i==0){
+            printf("%d\n",i);
+         }
+      }
+    return 0;
+}
+
+// int main(){
+//       int n;
+//       scanf("%d",&n);
+//       for( int i = 1; i<=n; i++){
+//          if(n/i==0){
+//             printf("%d\n",i);
+//          }
+//       }
+//     return 0;
+// }
